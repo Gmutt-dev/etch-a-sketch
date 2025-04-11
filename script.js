455bd71806f1d92a0c6d64883168ea0a3436bbd2
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-11 16:06:30 +0200

    added logic to add initial 16 x 16 grid on DOM
    created variable gridSize = 16 x 16 for initial size of grid required
      by project.
    added logic to genBlock(), genRow(), drawGrid()
    added CSS rules for .container -> empty for now
        .row -> make flexbox container
        .block -> make flexbox item 1 0 auto to stretch equally
            -> starting width of 1 and height of auto to stretch vertically

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-//draw the grid of div blocks on the DOM
-function drawGrid(gridSize) {
-
-}
-
 //generate a block <div> and return
 function genBlock() {
+    //create a new block element in memory
+    const block = document.createElement("div");
+    //add class = "block" attribute for CSS
+    block.classList.add("block");
     
+    return block;
 }
 
-//generate a row of blocks and return array of <div>s
-function genRow() {
-    
+//generate a row of blocks and return as <div> element
+function genRow(gridSize) {
+    //create a new row element in memory
+    const row = document.createElement("div"); 
+    //add class = "row" attribute for CSS
+    row.classList.add("row");
+    //add blocks to the row
+    for (let i = 1; i <= gridSize; i++) {
+        block = genBlock(); // <div> container block
+        row.appendChild(block);
+    }
+    return row;
+}
+
+//draw the grid of div blocks on the DOM in .container <div>
+function drawGrid(gridSize) {
+    //for loop to draw rows === gridSize param
+    for (let i = 1; i <= gridSize; i++) {
+        row = genRow(gridSize); // flexbox container <div> that contains blocks
+        container.appendChild(row);
+    }
 }
 
 //SCRIPT ENTRY POINT
+//get the .container div from the DOM
+const container = document.querySelector(".container");
 //initialize page with startup page content with 16 x 16 grid of blocks
 let gridSize = 16;  // project requires initial grid to be 16 x 16
 drawGrid(gridSize);
