4200e47d4c45575fc7298d8fea9582b892f070e8
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-11 20:06:30 +0200

    add button to reset grid on DOM and then redraw grid
    <button class = "button"> placed at top of .html as required by project
      instructions.
    removeGrid() created and now called first from drawGrid() to start
      with an empty grid
    user input received and validated from prompt (max 100 as suggested
      by project) then call drawGrid(userinput)

--- script.js
+++ script.js
@@ -22,8 +22,16 @@ function genRow(gridSize) {
     return row;
 }
 
+function removeGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
 //draw the grid of div blocks on the DOM in .container <div>
 function drawGrid(gridSize) {
+    //reset (delete) the current grid (if any)
+    removeGrid();
     //for loop to draw rows === gridSize param
     for (let i = 1; i <= gridSize; i++) {
         row = genRow(gridSize); // flexbox container <div> that contains blocks
@@ -46,3 +54,14 @@ container.addEventListener("mouseover", (e) => {
         }
     }
 )
+
+//get the <button> from the DOM
+const button = document.querySelector(".button");
+
+button.addEventListener("click", (e) => {
+    do {
+        gridSize = Number.parseInt(prompt("Please enter the new number of squares per side? (max 100)", "16"));
+        if (!((gridSize <= 100) && (gridSize >= 1))) alert("invalid input, please try again"); //test if input is max 100
+    } while (!((gridSize <= 100) && (gridSize >= 1)));
+    console.log(gridSize);
+})
