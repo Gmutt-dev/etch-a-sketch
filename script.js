85f4d395e383d4f5f7e8f13ec8c7b2334d7f876b
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-11 15:29:50 +0200

    added container <div> to .html + added planned structure in .js with comments

---
+++ script.js
@@ -0,0 +1,19 @@
+//draw the grid of div blocks on the DOM
+function drawGrid(gridSize) {
+
+}
+
+//generate a block <div> and return
+function genBlock() {
+    
+}
+
+//generate a row of blocks and return array of <div>s
+function genRow() {
+    
+}
+
+//SCRIPT ENTRY POINT
+//initialize page with startup page content with 16 x 16 grid of blocks
+let gridSize = 16;  // project requires initial grid to be 16 x 16
+drawGrid(gridSize);
